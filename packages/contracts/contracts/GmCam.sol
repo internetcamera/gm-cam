b0e0c06341d2b8c0ccd4bfe0eb85237a7066a0d8
CJ Pais <cj@cjpais.com>
2021-11-02 16:18:44 -0400

    added in airdrop functionality.. can do at deploy or in contract

--- packages/contracts/contracts/GmCam.sol
+++ packages/contracts/contracts/GmCam.sol
@@ -32,6 +32,7 @@ contract GmCam is ERC721, Ownable, TrustedForwarderRecipient {
     uint256 private _tokenIdCounter;
     mapping(uint256 => TokenData) public gmData;
     mapping(address => uint256) public filmBalances;
+    bool private _canAirdrop;
 
     // * CONSTRUCTOR * //
     constructor(address trustedForwarderAddress_)
@@ -39,13 +40,27 @@ contract GmCam is ERC721, Ownable, TrustedForwarderRecipient {
         TrustedForwarderRecipient(trustedForwarderAddress_)
     {
         address msgSender = _msgSender();
-        for (uint256 i = 1; i <= 100; i++) {
-            gmData[i].originalOwner = msgSender;
-            gmData[i].expiresAt = block.timestamp + (100 * 365 days);
-            filmBalances[msgSender] += 1;
-            emit FilmCreated(gmData[i].originalOwner, i, gmData[i].expiresAt);
-        }
-        _tokenIdCounter = 100;
+        // for (uint256 i = 1; i <= 100; i++) {
+        //     gmData[i].originalOwner = msgSender;
+        //     gmData[i].expiresAt = block.timestamp + (100 * 365 days);
+        //     filmBalances[msgSender] += 1;
+        //     emit FilmCreated(gmData[i].originalOwner, i, gmData[i].expiresAt);
+        // }
+        _tokenIdCounter = 1;
+        _canAirdrop = true;
+    }
+
+    function stopAirdrops() public onlyOwner {
+        _canAirdrop = false;
+    }
+
+    function airdropTo(address addr) public onlyOwner {
+        require(_canAirdrop, "Cannot airdrop");
+        gmData[i].originalOwner = addr;
+        gmData[i].expiresAt = block.timestamp + (100 * 365 days);
+        filmBalances[addr] += 1;
+        _tokenIdCounter += 1;
+        emit FilmCreated(gmData[i].originalOwner, i, gmData[i].expiresAt);
     }
 
     // * PUBLIC GM FUNCTIONS * //
