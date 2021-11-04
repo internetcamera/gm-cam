93acfd591b6643eb3eaa0aac07dc7d54c78b8c7e
shahruz <hello@shahr.uz>
2021-11-04 16:55:48 -0700

    contract update

--- packages/contracts/contracts/GmCam.sol
+++ packages/contracts/contracts/GmCam.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/*
+            :
+   `.       ;        .'
+     `.  .-'''-.   .'
+       ;'  __   _;'
+      /   '_    _`\
+     |  _( a (  a  |               
+'''''| (_)    >    |-----   gm cam
+      \    \    / /
+       `.   `--'.'
+      .' `-,,,-' `.
+    .'      :      `. 
+            :
+*/
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
